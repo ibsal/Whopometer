cbc993794e370438af2ac789fa523c77f0423968
Isaac Sal <isaacbsal@gmail.com>
2025-02-23 19:01:21 -0600

    Added high G accelerometer driver

--- CUBE/Core/Src/main.c
+++ CUBE/Core/Src/main.c
@@ -20,6 +20,7 @@
 #include "main.h"
 #include "fatfs.h"
 #include "usb_device.h"
+#include "h3lis331dl_reg.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -66,7 +67,111 @@ static void MX_I2C2_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
-uint8_t *data = "Its whopping time\n";
+I2C_HandleTypeDef hi2c2;
+I2C_HandleTypeDef hi2c1;
+
+uint8_t *data = "OHNO\n";
+uint8_t *datayay = "YAY";
+uint8_t checkval;
+int mpustatus = 0;
+
+//IDK WHAT THIS DOES
+#define MPU6050_ADDR 0xD0
+
+
+#define SMPLRT_DIV_REG 0x19
+#define GYRO_CONFIG_REG 0x1B
+#define ACCEL_CONFIG_REG 0x1C
+#define ACCEL_XOUT_H_REG 0x3B
+#define TEMP_OUT_H_REG 0x41
+#define GYRO_XOUT_H_REG 0x43
+#define PWR_MGMT_1_REG 0x6B
+#define WHO_AM_I_REG 0x75
+
+int16_t Accel_X_RAW = 0;
+int16_t Accel_Y_RAW = 0;
+int16_t Accel_Z_RAW = 0;
+
+int16_t Gyro_X_RAW = 0;
+int16_t Gyro_Y_RAW = 0;
+int16_t Gyro_Z_RAW = 0;
+
+float Ax, Ay, Az, Gx, Gy, Gz;
+
+
+void MPU6050_Init (void)
+{
+	uint8_t check;
+	uint8_t Data;
+
+	// check device ID WHO_AM_I
+	HAL_I2C_Mem_Read (&hi2c2, MPU6050_ADDR, 0x75, 1, &check, 1, 1000);
+	checkval = check;
+	if (check == 0x68)  // 0x68 will be returned by the sensor if everything goes well
+	{
+		mpustatus = 1;
+		// power management register 0X6B we should write all 0's to wake the sensor up
+		Data = 0;
+		HAL_I2C_Mem_Write(&hi2c2, MPU6050_ADDR, 0x6B, 1,&Data, 1, 1000);
+
+		// Set DATA RATE of 1KHz by writing SMPLRT_DIV register
+		Data = 0x07;
+		HAL_I2C_Mem_Write(&hi2c2, MPU6050_ADDR, 0x19, 1, &Data, 1, 1000);
+
+		// Set Gyroscopic configuration in GYRO_CONFIG Register
+		Data = 0x00;  // XG_ST=0,YG_ST=0,ZG_ST=0, FS_SEL=0 -> ± 250 ̐/s
+		HAL_I2C_Mem_Write(&hi2c2, MPU6050_ADDR, 0x1B, 1, &Data, 1, 1000);
+
+		// Set accelerometer configuration in ACCEL_CONFIG Register
+		Data = 0x00;  // XA_ST=0,YA_ST=0,ZA_ST=0, FS_SEL=0 -> ± 2g
+		HAL_I2C_Mem_Write(&hi2c2, MPU6050_ADDR, 0x1C, 1, &Data, 1, 1000);
+	}
+
+}
+
+
+void MPU6050_Read_Accel (void)
+{
+	uint8_t Rec_Data[6];
+
+	// Read 6 BYTES of data starting from ACCEL_XOUT_H (0x3B) register
+	HAL_I2C_Mem_Read (&hi2c2, MPU6050_ADDR, 0x3B, 1, Rec_Data, 6, 1000);
+
+	Accel_X_RAW = (int16_t)(Rec_Data[0] << 8 | Rec_Data [1]);
+	Accel_Y_RAW = (int16_t)(Rec_Data[2] << 8 | Rec_Data [3]);
+	Accel_Z_RAW = (int16_t)(Rec_Data[4] << 8 | Rec_Data [5]);
+
+	/*** convert the RAW values into acceleration in 'g'
+	     we have to divide according to the Full scale value set in FS_SEL
+	     I have configured FS_SEL = 0. So I am dividing by 16384.0
+	     for more details check ACCEL_CONFIG Register              ****/
+
+	Ax = (float)Accel_X_RAW/16384.0;
+	Ay = (float)Accel_Y_RAW/16384.0;
+	Az = (float)Accel_Z_RAW/16384.0;
+}
+
+
+void MPU6050_Read_Gyro (void)
+{
+	uint8_t Rec_Data[6];
+
+	// Read 6 BYTES of data starting from GYRO_XOUT_H register
+	HAL_I2C_Mem_Read (&hi2c2, MPU6050_ADDR, 0x43, 1, Rec_Data, 6, 1000);
+
+	Gyro_X_RAW = (int16_t)(Rec_Data[0] << 8 | Rec_Data [1]);
+	Gyro_Y_RAW = (int16_t)(Rec_Data[2] << 8 | Rec_Data [3]);
+	Gyro_Z_RAW = (int16_t)(Rec_Data[4] << 8 | Rec_Data [5]);
+
+	/*** convert the RAW values into dps (ｰ/s)
+	     we have to divide according to the Full scale value set in FS_SEL
+	     I have configured FS_SEL = 0. So I am dividing by 131.0
+	     for more details check GYRO_CONFIG Register              ****/
+
+	Gx = (float)Gyro_X_RAW/131.0;
+	Gy = (float)Gyro_Y_RAW/131.0;
+	Gz = (float)Gyro_Z_RAW/131.0;
+}
 
 /* USER CODE END 0 */
 
@@ -106,7 +211,8 @@ int main(void)
   MX_FATFS_Init();
   MX_USB_DEVICE_Init();
   /* USER CODE BEGIN 2 */
-
+  HAL_Delay(100);
+  //MPU6050_Init();
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -114,9 +220,51 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-	CDC_Transmit_FS(data, strlen(data));
-	HAL_Delay (1000);
-
+	  int found = 0;
+	 	        char charData[20]; // Data holder
+	 	        for(uint16_t i2c=1; i2c<128; i2c++)
+	 	        {
+	 	            if (HAL_I2C_IsDeviceReady(&hi2c1, i2c<<1, 5, 10) == HAL_OK)
+	 	            {
+	 	            found = 1;
+	 	            sprintf(charData, "%s : ", "i2cdevices.org");
+	 	                CDC_Transmit_FS((uint8_t *) charData, strlen(charData));
+	 	                HAL_Delay(500);
+	 	                sprintf(charData, "%x and ", i2c);
+	 	                CDC_Transmit_FS((uint8_t *) charData, strlen(charData));
+	 	                HAL_Delay(500);
+	 	                sprintf(charData, "%s : ", "STM32 Address");
+	 	                CDC_Transmit_FS((uint8_t *) charData, strlen(charData));
+	 	                HAL_Delay(500);
+	 	                sprintf(charData, "%x\n", i2c<<1);
+	 	                CDC_Transmit_FS((uint8_t *) charData, strlen(charData));
+	 	                HAL_Delay(500);
+	 	             }
+	 	             HAL_Delay(5);
+	 	             if (HAL_I2C_IsDeviceReady(&hi2c2, i2c<<1, 5, 10) == HAL_OK)
+	 	            	 	            {
+	 	            	 	            found = 1;
+	 	            	 	            sprintf(charData, "%s : ", "i2cdevices.org");
+	 	            	 	                CDC_Transmit_FS((uint8_t *) charData, strlen(charData));
+	 	            	 	                HAL_Delay(500);
+	 	            	 	                sprintf(charData, "%x and ", i2c);
+	 	            	 	                CDC_Transmit_FS((uint8_t *) charData, strlen(charData));
+	 	            	 	                HAL_Delay(500);
+	 	            	 	                sprintf(charData, "%s : ", "STM32 Address");
+	 	            	 	                CDC_Transmit_FS((uint8_t *) charData, strlen(charData));
+	 	            	 	                HAL_Delay(500);
+	 	            	 	                sprintf(charData, "%x\n", i2c<<1);
+	 	            	 	                CDC_Transmit_FS((uint8_t *) charData, strlen(charData));
+	 	            	 	                HAL_Delay(500);
+	 	            	 	             }
+	 	            	 	             HAL_Delay(5);
+	 	         }
+	 	         if (!found)
+	 	         {
+	 	              sprintf(charData, "%s", "No devices found!\n");
+	 	              CDC_Transmit_FS((uint8_t *) charData, strlen(charData));
+	 	              HAL_Delay(500);
+	 	         }
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
